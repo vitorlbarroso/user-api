3d5cd20b57afe0f97b3a3566d7d03fd474550bf8
vitorlbarroso <76413552+vitorlbarroso@users.noreply.github.com>
2022-09-13 14:39:44 -0300

    Padronizada as rotas e criada a database

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\PeopleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('users', [PeopleController::class, 'index']);
+
+Route::get('user/{id}', [PeopleController::class, 'store']);
+
+Route::post('user', [PeopleCpostroller::class, 'create']);
+
+Route::put('user/{id}', [PeopleCpostroller::class, 'update']);
+
+Route::delete('user/{id}', [PeopleCpostroller::class, 'destroy']);
